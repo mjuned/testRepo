9fa5dabeeca4662f3a57ebd0fb9512dae57b7030
juned.khan <juned3you@gmail.com>
2017-03-30 15:18:27 +0530

    testGradleproject

--- src/main/java/com/june/test/controller/TestController.java
+++ src/main/java/com/june/test/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping("/")
+    @RequestMapping("/test")
     public String index() {
         return "Greetings from Spring Boot!";
     }
